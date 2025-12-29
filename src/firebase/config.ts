b8de50aa25238cbf6af0c56ffb5a4eaab658292f
Pinsara Sasika Aluthge <Pinsaraaluthge@gmail.com>
2025-12-29 19:27:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-701628964-d453e",
+  "appId": "1:269338754094:web:bf09a75940ebedeba0b3bf",
+  "apiKey": "AIzaSyC0s8lf-ZtVp0aW25XDJd23V-Joff370Kw",
+  "authDomain": "studio-701628964-d453e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "269338754094"
+};
